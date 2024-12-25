f2b1e58f9a84caafcba8bd656c502d63340e2480
Kazi Rakibul Alam Tohin <sergeclein2006@gmail.com>
2024-12-25 14:16:56 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button
+const button = document.getElementById("clickMeBtn");
+
+// Add a click event listener
+button.addEventListener("click", () => {
+  alert("You clicked the button! Great job!");
+});
